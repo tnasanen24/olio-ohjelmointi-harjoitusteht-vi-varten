d1ed70441e6dc5811be156fbab974d22f80f3266
Tommy Näsänen <tommy.nasanen@gmail.com>
2025-11-30 20:14:45 +0200

    tehtava 5

---
+++ h5/classa2.h
@@ -0,0 +1,17 @@
+#ifndef CLASSA2_H
+#define CLASSA2_H
+
+#include "classb.h"
+
+
+class ClassA2
+{
+private:
+    ClassB &refB;
+public:
+    ClassA2(ClassB&); //Annetaan argumenttina B-luokan olion osoite
+    string getBinfo();
+    void setBinfo(string);
+};
+
+#endif // CLASSA2_H
---
+++ h5/main.cpp
@@ -0,0 +1,46 @@
+#include "classa1.h"
+#include "classa2.h"
+#include "classb.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int* pointerA = &a;
+    int &refA = a;
+
+    //&refA = b;
+    //pointerA = &b;
+
+    cout << "Muuttujan a osoite: " << &a << endl;
+    cout << "Muuttujan a arvo: " << a << endl;
+
+    cout << "Pointerin pointerA osoittama osoite: " << pointerA << endl;
+    cout << "Pointerin *pointerA osoittaman muistipaikan arvo: " << *pointerA << endl;
+
+    cout << "Referenssin &refA osoittama osoite: " << &refA << endl;
+    cout << "Referenssin refA osoittaman muistipaikan arvo: " << refA << endl;
+
+
+    ClassB objB;
+    objB.setInfo("Olion B asettama info");
+
+    ClassA1 objA1(objB);
+    objA1.setBinfo("Olion objA1 asettama info");
+
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objA1: "<<objA1.getBinfo()<<endl;
+
+
+    ClassB &refB=objB;
+    ClassA2 objA2(refB);
+    objA2.setBinfo("Olion Agr asettama info");
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objA2 "<<objA2.getBinfo()<<endl;
+    cout<<endl;
+    return 0;
+}
