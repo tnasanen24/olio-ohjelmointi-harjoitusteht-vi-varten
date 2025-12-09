acd6ec198c9ffe3677ca2eba1dd22b325830a78d
Tommy Näsänen <tommy.nasanen@gmail.com>
2025-12-09 18:55:09 +0200

    tehtava 6a

---
+++ h6a/main.cpp
@@ -0,0 +1,13 @@
+#include "myclass.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    MyClass object;
+    object.raiseMySignal();
+    return 0;
+}
